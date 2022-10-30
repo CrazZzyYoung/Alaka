722cfa0cc3a23098b638fde898dd7bff10c7cf49
CrazZzyYoung <kngstrass@gmail.com>
2022-10-30 18:21:41 +0300

    3

--- WindowsFormsApp3/Form1.Designer.cs
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -109,7 +109,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(212, 77);
+            this.button1.Location = new System.Drawing.Point(208, 141);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 8;
