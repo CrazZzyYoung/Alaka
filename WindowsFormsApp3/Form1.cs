4348e86b6bd2147f06b15619582e8d49e938ce71
CrazZzyYoung <kngstrass@gmail.com>
2022-10-30 18:21:17 +0300

    2

--- WindowsFormsApp3/Form1.cs
+++ WindowsFormsApp3/Form1.cs
@@ -16,5 +16,23 @@ namespace WindowsFormsApp3
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(textBox1.Text);
+            double b = Convert.ToDouble(textBox2.Text);
+            double c = Convert.ToDouble(textBox3.Text);
+
+            double q = Math.Round((-b + Math.Sqrt(b * b - 4 * a * c)) / (2 * a), 1);
+            double w = Math.Round((b + Math.Sqrt(b * b - 4 * a * c)) / (2 * a), 1);
+
+            label4.Text = $"Корень = {q.ToString()}";
+            label5.Text = $"Корень = {w.ToString()}";
+
+            label4.Visible = true;
+            label5.Visible = true;
+
+
+        }
     }
 }
